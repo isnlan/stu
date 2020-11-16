9dce5ba70bb7395c1541efd53aa84bdbad6c9eef
snlansky <snlan@live.cn>
2020-11-16 16:20:55 +0800

    update

--- gin_pg/main.go
+++ gin_pg/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,9 +17,44 @@ func main() {
 		})
 	})
 
-	router.NoRoute(HandleNotFound)
-	router.NoMethod(HandleNotFound)
-	router.Run("0.0.0.0:8081") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	router.GET("/ide/*action", func(c *gin.Context) {
+		action := c.Param("action")
+		fmt.Println("...", action)
+		action = strings.ReplaceAll(action, "/proxy", "")
+		director := func(req *http.Request) {
+			req.URL.Scheme = "http"
+			req.URL.Host = "dapp-5face284f6add4bd611d2264.dapp-project:3000"
+			req.URL.Path, req.URL.RawPath = action, action
+			if _, ok := req.Header["User-Agent"]; !ok {
+				// explicitly disable User-Agent so it's not set to default value
+				req.Header.Set("User-Agent", "")
+			}
+		}
+
+		proxy := &httputil.ReverseProxy{Director: director}
+		proxy.ServeHTTP(c.Writer, c.Request)
+	})
+
+	//router.GET("/ide", func(c *gin.Context) {
+	//	director := func(req *http.Request) {
+	//		req.URL.Scheme = "http"
+	//		req.URL.Host = "dapp-5face284f6add4bd611d2264.dapp-project:3000"
+	//		req.URL.Path, req.URL.RawPath = "", ""
+	//		//if targetQuery == "" || req.URL.RawQuery == "" {
+	//		//	req.URL.RawQuery = targetQuery + req.URL.RawQuery
+	//		//} else {
+	//		//	req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+	//		//}
+	//		if _, ok := req.Header["User-Agent"]; !ok {
+	//			// explicitly disable User-Agent so it's not set to default value
+	//			req.Header.Set("User-Agent", "")
+	//		}
+	//	}
+	//	proxy := &httputil.ReverseProxy{Director: director}
+	//	proxy.ServeHTTP(c.Writer, c.Request)
+	//})
+
+	router.Run()
 }
 
 func HandleNotFound(c *gin.Context) {
